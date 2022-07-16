37a58e5bffe2b13eef24e20b93ee9f5e16200d8f
As'ad Saleh Umar <asad@infradigital.io>
2022-07-16 10:56:12 +0700

    Apply new prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View
       style={{
-        backgroundColor: "white",
+        backgroundColor: 'white',
         flex: 1,
-        alignContent: "center",
-        justifyContent: "center",
+        alignContent: 'center',
+        justifyContent: 'center',
       }}>
-      <Text style={{ textAlign: "center" }}>
+      <Text style={{ textAlign: 'center' }}>
         Welcome to Flip React Native App
       </Text>
     </View>
